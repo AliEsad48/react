28f17f96c8630202713b8d1bff111e6b46ff94a6
Salih Bayram <alicamci2020@gmail.com>
2021-10-07 21:27:29 +0300

    V14

--- 01-my-app/src/components/clock1/Clock1.js
+++ 01-my-app/src/components/clock1/Clock1.js
@@ -2,13 +2,16 @@ import "./clock1.css";
 import moment from "moment";
 
 const Clock1 = () => {
-    return <div className="clock-container">
+    const dateTime = moment();
+
+    return ( <div className="clock-container">
         <div className="clock">20:11</div>
         <div>
             <div className="date">AUGUST 13,2021</div>
             <div className="day">FRIDAY EVENING</div>
         </div>
-    </div>;
+    </div>
+    );
 };
 
 export default Clock1;
