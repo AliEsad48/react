ccd5009933a6128e76b6407b63b47df91756c1f7
Salih Bayram <alicamci2020@gmail.com>
2021-10-07 21:46:01 +0300

    V15

--- 01-my-app/src/components/clock1/Clock1.js
+++ 01-my-app/src/components/clock1/Clock1.js
@@ -1,16 +1,29 @@
 import "./clock1.css";
 import moment from "moment";
+import 'moment/locale/tr';
 
 const Clock1 = () => {
     const dateTime = moment();
+    const timeStr = dateTime.format( "HH:mm" )
+    const dateStr = dateTime.format( "LL" )
+    const dayStr = dateTime.format( "dddd" )
+    const hour = dateTime.format( "HH" )
+    let message = "";
+    if ( hour > 6 && hour < 11 ) message = "Gündüz";
+    else if ( hour >= 11 && hour < 17 ) message = "Öğlen";
+    else if ( hour >= 17 && hour < 21 ) message = "Akşam"
+    else message = "Gece";
 
-    return ( <div className="clock-container">
-        <div className="clock">20:11</div>
-        <div>
-            <div className="date">AUGUST 13,2021</div>
-            <div className="day">FRIDAY EVENING</div>
+
+
+    return (
+        <div className="clock-container">
+            <div className="clock">{timeStr}</div>
+            <div>
+                <div className="date">{dateStr}</div>
+                <div className="day">{dayStr} {message}</div>
+            </div>
         </div>
-    </div>
     );
 };
 
