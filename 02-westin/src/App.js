00e15dc8e303bd0e3791eb0c934494f49832cdbf
Salih Bayram <alicamci2020@gmail.com>
2021-10-08 23:01:22 +0300

    V24

--- 02-westin/src/App.js
+++ 02-westin/src/App.js
@@ -3,6 +3,8 @@ import Preloader from "./components/Preloader";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
 import About from "./components/About";
+import Services from "./components/Services";
+import Portfolio from "./components/Portfolio";
 const App = () => {
   return (
     <div>
@@ -10,6 +12,8 @@ const App = () => {
       <Header />
       <Banner />
       <About />
+      <Services />
+      <Portfolio />
     </div>
   );
 };
