0a3c76cf3a62af7a29c06ee37d1b17393203d025
Salih Bayram <alicamci2020@gmail.com>
2021-10-11 22:18:01 +0300

    V29

--- 01-my-app/src/App.js
+++ 01-my-app/src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
+import BSClassic from "./components/bootstrap-classic/BSClassic";
+import BSReact from "./components/bootstrap-react/BSReact";
 import Clock1 from "./components/clock1/Clock1";
 import Clock2 from "./components/clock2/Clock2";
 import Hello from "./components/hello/Hello";
+import Image from "./components/image/Image";
 import Jsx from "./components/jsx/Jsx";
 import Profile from "./components/profile/Profile";
 import Props from "./components/props/Props";
-import ExternalStyle from "./style/ExternalStyle";
-import InlineStyle from "./style/InlineStyle";
-import InternalStyle from "./style/InternalStyle";
+// import ExternalStyle from "./components/style/ExternalStyle";
+// import InlineStyle from "./components/style/InlineStyle";
+// import InternalStyle from "./components/style/InternalStyle";
 import Test from "./components/test/Test";
+
+
 function App() {
   const avatarUrl =
     "https://images.unsplash.com/photo-1633156583772-3c3ccee99b91?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80";
+
   return (
     <div>
       {/*
@@ -22,35 +28,61 @@ function App() {
       <Jsx />
       <InlineStyle />
       <InternalStyle />
-     // <ExternalStyle />
+      <ExternalStyle />
  
       <Clock1 />
+
       
+
       <Props ad="Ziya" soyad="Sarı" />
       <Props ad="Ali" soyad="Kara" />
       <Props ad="Ayşe" soyad="Pembe" />
       <Props ad="Hakan" soyad="Mavi" />
+
        
+
       <Clock2 yaziRengi="#FFFFFF" zeminRengi="#000000" />
      
-*/}
-      <Profile
-        avatar={avatarUrl}
-        name="Ali Gel"
-        location="Turkiye, İstanbul"
-        shot="4"
-        followers="25"
-        following="45"
-      />
-      <Profile
-        avatar="https://images.unsplash.com/photo-1550985607-b636cef44ec4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=535&q=80"
-        name="Mehmet Gel"
-        location="Turkiye, İzmir"
-        shot="43"
-        followers="123"
-        following="4545"
-      />
-    </div>
+
+
+      <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+        <Profile
+          avatar={avatarUrl}
+          name="Ali Gel"
+          location="Turkiye, İstanbul"
+          shot="4"
+          followers="25"
+          following="45"
+        />
+
+        <Profile
+          avatar="https://images.unsplash.com/photo-1550985607-b636cef44ec4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=535&q=80"
+          name="Mehmet Gel"
+          location="Turkiye, İzmir"
+          shot="43"
+          followers="123"
+          following="4545"
+        />
+
+        <Profile
+          avatar="https://images.unsplash.com/photo-1550985607-b636cef44ec4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=535&q=80"
+          name="Mehmet Gel"
+          location="Turkiye, İzmir"
+          shot="43"
+          followers="123"
+          following="4545"
+        />
+      </div>
+
+     
+
+      <Image />
+       
+       <BSClassic />
+    */}
+    <BSReact />
+       </div>
   );
 }
+
 export default App;
