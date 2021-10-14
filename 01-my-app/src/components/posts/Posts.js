f77ea70fd30891290a4f3f86809575536cd55669
Salih Bayram <alicamci2020@gmail.com>
2021-10-14 23:28:33 +0300

    V50

--- 01-my-app/src/components/posts/Posts.js
+++ 01-my-app/src/components/posts/Posts.js
@@ -1,10 +1,24 @@
 import axios from "axios"
 import React, { useState, useEffect } from "react"
-import { Container } from "react-bootstrap"
+import { Button, Container } from "react-bootstrap"
 import Post from "./Post"
 
 const Posts = () => {
   const [posts, setPosts] = useState([])
+
+  const ekle = () => {
+    const yeniYazi = {
+      title: "Merhaba Dünya",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis repudiandae corrupti aliquid nisi consectetur fuga id sequi saepe, velit eos dolorem culpa? Consectetur accusamus quisquam dicta. Et maiores tenetur magnam?",
+      userId: 1,
+    }
+
+    axios
+      .post("https://jsonplaceholder.typicode.com/posts", yeniYazi)
+      .then((resp) => {
+        setPosts([yeniYazi, ...posts])
+      })
+  }
 
   useEffect(() => {
     axios("https://jsonplaceholder.typicode.com/posts").then((resp) => {
@@ -14,6 +28,9 @@ const Posts = () => {
 
   return (
     <Container>
+      <Button variant="info" className="mb-4 mt-4" onClick={ekle}>
+        Yeni Ekle
+      </Button>
       {posts.map((post) => (
         <Post baslik={post.title} mesaj={post.body} key={post.id} />
       ))}
