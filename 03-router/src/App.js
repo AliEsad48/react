00313db5e4231a9eb4296ebba0badc2e7043efab
Salih Bayram <alicamci2020@gmail.com>
2021-10-16 23:05:30 +0300

    V61

--- 03-router/src/App.js
+++ 03-router/src/App.js
@@ -1,7 +1,31 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
+import Footer from "./components/shared/Footer"
+import Header from "./components/shared/Header"
+import Menu from "./components/shared/Menu"
+import AboutPage from "./pages/AboutPage"
+import ContactPage from "./pages/ContactPage"
+import HomePage from "./pages/HomePage"
 
 function App() {
-  return  <div className="App">Merhaba Dünya </div>;
+  return (
+    <Router>
+      <Header />
+      <Menu />
+      <Switch>
+        <Route path="/">
+          <HomePage />
+        </Route>
+        <Route path="/about">
+          <AboutPage />
+        </Route>
+        <Route path="/contact">
+          <ContactPage />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
